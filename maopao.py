01a407f533375c8b272589355fa3a3f708cfeddd
Rice <ones@xifandeMacBook-Pro.local>
2022-04-27 17:39:39 +0800

    first commit

---
+++ maopao.py
@@ -0,0 +1,11 @@
+#coding = utf-8
+
+list1 = [10,5,7,38,11,2,4,3]
+
+for s in range(len(list1)):
+    for i in range(len(list1)- s - 1):
+        j = i + 1
+        if list1[i] > list1[j]:
+            list1[i], list1[j] = list1[j], list1[i]
+            print(list1)
+# print(list1)
